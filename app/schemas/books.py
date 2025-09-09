2f9f6c6fd4eace26945c0a6b67f206fe4bdc8297
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-09-10 00:19:56 +0530

    book_schema.py

---
+++ app/schemas/books.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class BookBase(BaseModel):
+    title: str
+    description: str | None = None
+
+
+class BookCreate(BookBase):
+    pass
+
+
+class BookUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+
+
+class BookResponse(BookBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
