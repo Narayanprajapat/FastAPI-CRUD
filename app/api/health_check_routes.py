fc916b524673c369f8e1d731c7ba6f3d016530bb
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-09-10 00:19:26 +0530

    health_router.py

---
+++ app/api/health_check_routes.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, status
+from app.schemas.api_response import Response
+from app.core.enums import ResponseMessages
+
+
+health_check_router = APIRouter()
+
+
+@health_check_router.get(
+    path="/health", response_model=Response, status_code=status.HTTP_200_OK
+)
+async def health():
+    return Response(status_code=status.HTTP_200_OK, message=ResponseMessages.Success)
