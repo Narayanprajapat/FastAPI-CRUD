8424b3eb0ec31130142a609e1099d01a66a66ee7
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-09-10 15:19:07 +0530

    added api key dependencies

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from app.core.security import get_api_key
 from app.api.book_routes import book_router
 from app.api.health_check_routes import health_check_router
 
 routers = APIRouter()
 
 
-routers.include_router(router=book_router, prefix="/items", tags=["books"])
+routers.include_router(
+    router=book_router,
+    prefix="/books",
+    tags=["books"],
+    dependencies=[Depends(get_api_key)],
+)
 routers.include_router(router=health_check_router, tags=["Health"])
