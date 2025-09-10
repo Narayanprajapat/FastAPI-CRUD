bd984e8464a44bb0e23b7346af8e5c658449bc42
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-09-10 16:06:58 +0530

    env.py

---
+++ migrations/env.py
@@ -0,0 +1,66 @@
+import asyncio
+from alembic import context
+from sqlalchemy import pool
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+
+from app.db.base import Base
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+
+DATABASE_URL = "postgresql+asyncpg://postgres:1999@localhost:5432/postgres"
+
+
+target_metadata = Base.metadata
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
+
+def run_migrations_offline():
+    context.configure(
+        url=DATABASE_URL,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online():
+    connectable = AsyncEngine(
+        engine_from_config(
+            config.get_section(config.config_ini_section, {}),
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+            future=True,
+        )
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
